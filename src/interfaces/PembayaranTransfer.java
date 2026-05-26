a3eee4da912a4538f317db6196d5642117fd5f6f
Bakut321 <belbur321@gmail.com>
2026-05-26 23:00:29 +0700

    add PembayaranTransfer class

---
+++ src/interfaces/PembayaranTransfer.java
@@ -0,0 +1,16 @@
+package interfaces;
+
+public class PembayaranTransfer implements PembayaranService {
+
+    @Override
+    public void prosesPembayaran() {
+        System.out.println("Pembayaran transfer sedang diproses...");
+        System.out.println("Pembayaran transfer berhasil.");
+    }
+
+    @Override
+    public boolean validasiPembayaran() {
+        System.out.println("Memvalidasi pembayaran transfer...");
+        return true;
+    }
+}
