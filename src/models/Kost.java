aa87235d1a1a3254b4f25aaeefc3b3d3a741b3ff
Bakut321 <belbur321@gmail.com>
2026-05-26 22:27:06 +0700

    update Kost

--- src/models/Kost.java
+++ src/models/Kost.java
@@ -3,7 +3,6 @@ package models;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Kost {
     private String namaKost;
     private String alamat;
@@ -15,10 +14,21 @@ public class Kost {
     }
 
     public void tambahKamar(Kamar kamar) {
-
+        if (kamar == null) {
+            throw new IllegalArgumentException("Data kamar tidak boleh null!");
+        }
+        daftarKamar.add(kamar);
+        System.out.println("Kamar berhasil ditambahkan.");
     }
 
     public void hapusKamar(int id) {
-        
+        for (int i = 0; i < daftarKamar.size(); i++) {
+            if (daftarKamar.get(i).getIdKamar() == id) {
+                daftarKamar.remove(i);
+                System.out.println("Kamar berhasil dihapus.");
+                return;
+            }
+        }
+        System.out.println("Kamar dengan ID " + id + " tidak ditemukan.");
     }
 }
