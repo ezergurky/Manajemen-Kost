c80fe51f80e9ce10fd9d79f8f012343f69817578
Bakut321 <belbur321@gmail.com>
2026-06-28 23:56:22 +0700

    Manajemen Kost

--- src/models/Kost.java
+++ src/models/Kost.java
@@ -4,43 +4,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Kost {
+    private int idKost;
     private String namaKost;
     private String alamat;
     private List<Kamar> daftarKamar = new ArrayList<>();
 
-    public Kost(String namaKost, String alamat) {
+    public Kost(int idKost, String namaKost, String alamat) {
+        this.idKost = idKost;
         this.namaKost = namaKost;
         this.alamat = alamat;
     }
 
-    public void tambahKamar(Kamar kamar) {
-        if (kamar == null) {
-            throw new IllegalArgumentException("Data kamar tidak boleh null!");
-        }
-        daftarKamar.add(kamar);
-        System.out.println("Kamar berhasil ditambahkan.");
+    public int getIdKost() {
+        return idKost;
     }
 
-    public void hapusKamar(int id) {
-        for (int i = 0; i < daftarKamar.size(); i++) {
-            if (daftarKamar.get(i).getIdKamar() == id) {
-                daftarKamar.remove(i);
-                System.out.println("Kamar berhasil dihapus.");
-                return;
-            }
-        }
-        System.out.println("Kamar dengan ID " + id + " tidak ditemukan.");
+    public void setIdKost(int idKost) {
+        this.idKost = idKost;
     }
 
     public String getNamaKost() {
         return namaKost;
     }
 
+    public void setNamaKost(String namaKost) {
+        this.namaKost = namaKost;
+    }
+
     public String getAlamat() {
         return alamat;
     }
 
-    public List<Kamar> getDaftarKamar() {
-        return daftarKamar;
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setDaftarKamar(List<Kamar> daftarKamar) {
+        this.daftarKamar = daftarKamar; 
+    }
+    
+    public List<Kamar> getDaftarKamar() { 
+        return daftarKamar; 
     }
 }
